1e945d89eca46633dc4f9720531d8af7fd11e03a
Lishajins <lishajins2006@gmail.com>
2025-06-14 16:55:27 +0530

    fixed cat

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,10 @@
+import catLogo from '../assets/github-logo-png.png'
+
 function Header(){
     return(
         <div className="header">
         <h1 className="title">GitFinder</h1>
-         <img className="cat" src="./src/assets/github-logo-png.png"></img>
+         <img className="cat" src={catLogo}></img>
         </div>
     );
 }
